7d80712859b9bf622ece127b28922135d4ab085a
Andrejlebedev1998 <andrej_lebedev98@mail.ru>
2022-09-16 01:47:06 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/* let persone = {
+  name: "John",
+  age: 25,
+  isMarried: true,
+};
+
+console.log(persone["age"]);
+
+/* let arr = ["plum.png", "orange.jpg", "apple.bmp"];
+
+console.log(arr[1]); */
+
+/* let answer = +prompt("Есть ли вам 18 лет?", "Да");
+
+console.log(typeof answer); */
+
+/* let incr = 10,
+  decr = 10;
+
+console.log(incr++, decr--);
+
+console.log("2" === 2); */
+
+let isChecked = true,
+  isClose = false;
+
+console.log(isChecked && !isClose);
